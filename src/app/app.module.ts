51cd257a94e45e83e5388ff62b519a382614b9b4
Dave <dave@webdave.de>
2019-01-10 09:26:53 +0100

    nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutModule } from './about/about.module';
 import { BooksModule } from './books/books.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
