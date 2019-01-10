b2ead4d284ecba50ad4075707f58a28f1a34e47b
Dave <dave@webdave.de>
2019-01-10 08:48:32 +0100

    sub modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: "app-root",
+  selector: "wd1-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   title = "workshop UHU";
+  constructor(private service: Title) {
+    console.log(service);
+    service.setTitle("FooHuHu");
+  }
 }
---
+++ src/app/books/books/books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wd-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
+})
+export class BooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
